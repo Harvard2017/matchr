6f23d268d9edee0173a1e47d6f1b805ddfa32b19
Allan Wang <me@allanwang.ca>
2017-10-20 23:39:25 -0400

    Update some implementations

--- app/src/main/java/com/matchr/Interfaces.kt
+++ app/src/main/java/com/matchr/Interfaces.kt
@@ -10,8 +10,18 @@ interface IQuestionView {
     fun onResponse(): List<String>
 }
 
+/**
+ * A single question that handles view generation
+ * and propagation
+ */
 interface IQuestion {
+    /**
+     * Type, which helps define how the data is handled
+     */
     val type: QuestionType
+    /**
+     * Strictly unique
+     */
     val key: Int
     val weight: Float
     val isSkippable: Boolean
@@ -20,7 +30,32 @@ interface IQuestion {
     fun onResponse(response: Response): IQuestion
 }
 
-interface Matcher {
-    fun match(response: Response, otherResponse: Response): Float
-    fun onMatchless(response: Response): Float
+/**
+ * Global handler to compare data sets
+ * Implementation allows the option to handle data through enums
+ */
+interface Matcher<K> {
+
+    /**
+     * Get key of a response to be mapped
+     */
+    fun map(response: Response): Pair<K, Response>
+
+    /**
+     * Matches the corresponding keys
+     * For instance, a question asking for time schedules should map the same types
+     * A question pair such as "describe yourself" and "what do you look for in a person?" should map to different types
+     *
+     * The intent is to map key codes together, though the base implementation
+     * is a list to allow for multiple matches for the same key
+     */
+    val responseMapper: List<Pair<K, K>>
+
+    /**
+     * Match two complete data sets
+     * This should be handled by a delegate
+     */
+    fun matchAllResponses(responses: Map<K, Response>, otherResponses: Map<K, Response>): Float
+    
+    fun matchResponse()
 }
