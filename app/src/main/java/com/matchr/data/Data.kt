4f03da0a563ce9ca868e39be7e4210dd9399f86c
Allan Wang <me@allanwang.ca>
2017-10-21 15:56:52 -0400

    Cleanup (#3)

--- app/src/main/java/com/matchr/data/Data.kt
+++ app/src/main/java/com/matchr/data/Data.kt
@@ -1,22 +1,33 @@
 package com.matchr.data
 
-import com.matchr.fragments.QuestionFragment
-import com.matchr.fragments.ShortAnswerFragment
+import ca.allanwang.kau.ui.widgets.TextSlider
+import com.matchr.R
+import com.matchr.iitems.ChoiceItem
+import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter
 
 /**
  * Created by Allan Wang on 2017-10-20.
  */
 enum class QuestionType(
-        val fragmentFactory: () -> QuestionFragment
+        val layoutRes: Int, private val withMultiSelect: Boolean
 ) {
-    SINGLE_CHOICE({ ShortAnswerFragment() }),
-    MULTIPLE_CHOICE({ ShortAnswerFragment() }),
-    LIST({ ShortAnswerFragment() }),
-    SHORT_ANSWER({ ShortAnswerFragment() }),
-    LONG_ANSWER({ ShortAnswerFragment() });
+    SINGLE_CHOICE(R.layout.iitem_choice_single, false),
+    MULTIPLE_CHOICE(R.layout.iitem_choice_multi, true);
 
-    fun createFragment(question: IQuestion): QuestionFragment
-            = fragmentFactory().withQuestion(question)
+    fun updateAdapter(question: Question, title: TextSlider, adapter: FastItemAdapter<ChoiceItem>) {
+        title.setText(question.question)
+        adapter.set(question.options.map { ChoiceItem(this, it.first) })
+        adapter.withMultiSelect(withMultiSelect)
+    }
+
+    companion object {
+        val values = values()
+        operator fun invoke(index: Int) = values[index]
+    }
 }
 
 data class Response(val qOrdinal: Int, val data: List<String>)
+
+data class Question(val id: Int, val question: String, val options: List<Pair<String, Int>>, val type: Int) {
+    fun delegate() = QuestionType(type)
+}
