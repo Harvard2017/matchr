64dba81f933142a4f0e93d8f0e0938356e5b3870
BeckhamL <beckham.lam>
2017-10-21 14:19:40 -0400

    to firebase

--- app/src/main/java/com/matchr/LoginActivity.java
+++ app/src/main/java/com/matchr/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.matchr;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -13,17 +17,20 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A login screen that offers login via email/password.
  */
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
     private static final int REQ_CODE = 9001;
     private GoogleApiClient googleApiClient;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +41,86 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             @Override
             public void onClick(View v) {
                 signIn();
+
+            }
+        });
+
+        findViewById(R.id.bn_logout).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+
             }
         });
 
         GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
+
     }
 
     private void signIn() {
         Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
         startActivityForResult(intent, REQ_CODE);
+
+    }
+
+    private void signOut() {
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        Context context = getApplicationContext();
+                        Toast.makeText(context, "Logged out", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private void handleResult(GoogleSignInResult result) {
-        if (result.isSuccess()) {
+        // if successfully signed in with google
+        if (googleApiClient != null && googleApiClient.isConnected()) {
             GoogleSignInAccount account = result.getSignInAccount();
             // contains user ID
             String personId = account.getId();
+            String personName = account.getDisplayName();
+            String personEmail = account.getEmail();
+            String givenName = account.getGivenName();
+            String comb = "u" + givenName;
 
+            myRef.child(comb + "/UserId").setValue(personId);
+            myRef.child(comb + "/Name").setValue(personName);
+            myRef.child(comb + "/Email").setValue(personEmail);
+
+            // LoginActivity.class will be changed with next activity
+            Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+            intent.putExtra("UserId", personId);
+            startActivity(intent);
         }
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bn_logout:
+                signOut();
+                break;
+            case R.id.signin_button:
+                signIn();
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQ_CODE) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
     }
 }
